8185032fd40c736fbf1985a755dc9d8210f047da
Patrick Miller <paddymills@proton.me>
2024-05-17 20:46:40 -0400

    fix functions ts

--- functions/test.ts
+++ functions/test.ts
@@ -1,4 +1,6 @@
-export function onRequest(context: EventContext) {
+import { Context } from "./types";
+
+export function onRequest(context: Context) {
   console.log(context);
   return new Response("Hello from the test function!");
 }

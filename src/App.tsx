75c7ff37f7ca2af0b3a6d82e734b0231b6a2d79b
Patrick Miller <paddymills@proton.me>
2024-06-29 16:42:24 -0400

    add tailwind styles, header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-import { For, createSignal, type Component } from 'solid-js';
-import { createStore } from 'solid-js/store';
-
-import styles from './App.module.css';
+import { For, createSignal, type Component } from "solid-js";
+import { createStore } from "solid-js/store";
 
 type TodoItem = { value: string };
 
+import box from './assets/box.svg';
+
 const App: Component = () => {
-  const [val, setVal] = createSignal<string>('');
-  const [todos, setTodos] = createStore<TodoItem[]>([]);
+  const [val, setVal] = createSignal<string>("");
+  const [todos, setTodos] = createStore<TodoItem[]>([
+    { value: "make dinner" },
+    { value: "do dishes" },
+    { value: "do amazing thing" },
+  ]);
 
   const addTodo = (todo: string) => {
-    setTodos(l => [...l, { value: todo }])
-    setVal('');
-  }
+    setTodos((l) => [...l, { value: todo }]);
+    setVal("");
+  };
 
   return (
-    <div class={styles.App}>
-        <h1>Lists</h1>
-        <input
-            onChange={e => addTodo(e.target.value)}
-            onInput={e => setVal(e.currentTarget.value)}
+    <>
+      <header class="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-4">
+        <p>too many lists...</p>
+      </header>
+      <main class="grow flex items-center place-content-center">
+        <div class="grid grid-cols-2 gap-2 rounded-lg bg-gradient-to-tr from-cyan-500 to-blue-500 p-4">
+          <input
+            class="col-span-full rounded px-2 py-1"
+            onChange={(e) => addTodo(e.target.value)}
+            onInput={(e) => setVal(e.currentTarget.value)}
             value={val()}
-            placeholder='enter todo'
+            placeholder="enter todo"
             required
-        />
-        <ul>
-            <For each={todos}>{(todo) => <li>{todo.value}</li>}</For>
-        </ul>
-    </div>
+          />
+          <For each={todos}>
+            {(todo) => (
+              <>
+                <img class="col-start-1 size-4" src={box}/>
+                <p>{todo.value}</p>
+              </>
+            )}
+          </For>
+        </div>
+      </main>
+      <footer class="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-4">
+        <p class="text-end">made with <span class="text-red-500">&hearts;</span> by Patrick Miller</p>
+      </footer>
+    </>
   );
 };
 
